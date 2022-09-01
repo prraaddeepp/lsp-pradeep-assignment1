7eaae5bba2eeee24e95d18e7a12c10f0485fe7a7
Pradeep Lamichhane <lcpradip2000@gmail.com>
2022-09-01 14:28:21 -0400

    This is my first commit for the assignment 1.

---
+++ lsp-pradeep-assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+/*
+ * pradeep lamichhane
+ * @author prraaddeepp
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
